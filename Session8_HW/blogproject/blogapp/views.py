6cb738245c33ad141c7fa5359158003f1c48f1e6
toto9602 <toto9602@naver.com>
2021-05-13 13:54:12 +0900

    commit_S8

---
+++ Session8_HW/blogproject/blogapp/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('date')   #마감 기한 적게 남은 순서대로 정렬하기????
+
+    return render(request, 'home.html', {'posts' : posts})
+
+def new(request):
+    if request.method == 'POST':
+        new_post = Post.objects.create(
+            title = request.POST['title'], #method == post 안에서 name이 title인 것 in templates
+            content = request.POST['content'],
+            date = request.POST['date']
+        )
+        return redirect('detail', new_post.pk)
+
+    return render(request, 'new.html')
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.method == 'POST':
+        content = request.POST['content']
+        Comment.objects.create(
+            post=post,
+            content=content
+        )
+        return redirect('detail', post_pk)
+
+    return render(request, 'detail.html', {'post':post})
+
+def edit(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.method == 'POST':
+        Post.objects.filter(pk=post_pk).update(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            date = request.POST['date']
+        )
+        return redirect('detail', post_pk)
+
+    return render(request, 'edit.html', {'post' : post})
+
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+
+    return redirect('home')
+
+def delete_comment(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('detail', post_pk)
